1ace1425855ab875e0819e44982ed07211db6260
sarvesh-gadgil <60133683+sarvesh-gadgil@users.noreply.github.com>
2020-06-26 22:35:23 +0530

    Added styling for login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import { login } from '../actions/action';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
+import avatar_img from '../images/dublin_bus.png';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 class Login extends React.Component {
 
@@ -25,10 +34,77 @@ class Login extends React.Component {
     render() {
         return (
             <div>
-                Login Page < br />
-                <input type="text" name="username" onChange={this.handleLoginFormValues.bind(this)} value={this.state.credentials.username}></input>
-                <input type="password" name="password" onChange={this.handleLoginFormValues.bind(this)} value={this.state.credentials.password}></input>
-                <button type="button" onClick={this.loginUser.bind(this)}>Login</button>
+                <Container component="main" maxWidth="xs">
+                    <Grid
+                        container
+                        spacing={0}
+                        direction="column"
+                        alignItems="center"
+                        justify="center"
+                        style={{ minHeight: '75vh' }}
+                    >
+                        <Grid item>
+                            <CssBaseline />
+                            <Paper elevation={2} style={{ padding: "30px", height: "400px" }}>
+                                <Avatar style={{ marginLeft: "auto", marginRight: "auto", width: "50px", height: "50px" }} src={avatar_img} />
+                                <Typography component="h1" variant="h6" align="center">
+                                    Login to Dublin Bus
+                                </Typography>
+                                <form>
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        autoComplete="email"
+                                        size="small"
+                                        variant="outlined"
+                                        name="username"
+                                        onChange={this.handleLoginFormValues.bind(this)}
+                                        value={this.state.credentials.username}
+                                    // InputLabelProps={{
+                                    //     style: { color: 'gray' }
+                                    // }}
+                                    />
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        size="small"
+                                        variant="outlined"
+                                        name="password"
+                                        onChange={this.handleLoginFormValues.bind(this)}
+                                        value={this.state.credentials.password}
+                                    />
+                                    <br />
+                                    <br />
+                                    <Button
+                                        type="button"
+                                        fullWidth
+                                        variant="contained"
+                                        style={{ backgroundColor: '#2e7d32', color: 'white' }}
+                                        onClick={this.loginUser.bind(this)}
+                                    >
+                                        Login
+                                    </Button>
+                                    <br />
+                                    <br />
+                                    <Button
+                                        type="button"
+                                        fullWidth
+                                        variant="contained"
+                                    >
+                                        Register
+                                    </Button>
+                                </form>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Container>
             </div>
         );
     }
