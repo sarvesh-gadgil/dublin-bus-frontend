eddc8e42a5eef5034470f795aa467c2a3dcbe56a
sarvesh-gadgil <60133683+sarvesh-gadgil@users.noreply.github.com>
2020-07-13 17:45:19 +0530

    Fixed bug of banner images for various sizes

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import GoogleMap from '../components/GoogleMap';
-import home_banner from '../images/home_banner.jpg';
-// import login_banner from '../images/login_banner.jpg';
+// import home_banner from '../images/home_banner.jpg';
+import './Home.css'
 
 class Home extends React.Component {
     render() {
         return (
-            <div style={{
-                width: "100%",
-                height: "115vh",
-                backgroundImage: `url(${home_banner})`,
-                backgroundSize: 'cover',
-                padding: "8px"
-            }}>
+            // <div style={{
+            //     width: "100%",
+            //     height: "115vh",
+            //     backgroundImage: `url(${home_banner})`,
+            //     backgroundSize: 'cover',
+            //     padding: "8px"
+            // }}>
+            <div className="homeBanner">
                 <GoogleMap />
+                <br />
             </div>
         );
     }
